6a9579dba5832b0a338a6ceb2b61a6321ac81bcb
David <davidisams@gmail.com>
2022-12-26 13:52:10 -0600

    pitt crew radio reads speed of car

---
+++ lm_pitt/src/radio.h
@@ -0,0 +1,21 @@
+
+
+// SX1276 pin connections:
+//       | SLOT 0 | SLOT 1 |
+//==========================
+// cs    |   CS0  |   CS1  |
+// dio0  |   D0   |   D1   |
+// dio1  |   G2   |   G7   |
+// dio2  |   G3   |   G8   |
+// rst   |   G1   |   G6   |
+// tx_en |  PWM0  |  PWM1  |
+// rx_en |   G0   |   G5   |
+
+
+    int pin_cs =        5;  
+    int pin_dio0 =      14;         //14
+    int pin_tx_enable = 13;       //13
+    int pin_rx_enable = 15;         //15
+    int pin_nrst =      25;         //25
+    int pin_dio1 =      26;         //26
+ 
