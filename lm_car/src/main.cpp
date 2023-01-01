131b31f2fe812b006af40e312c8d6be303120068
David <davidisams@gmail.com>
2023-01-01 15:01:34 -0600

    added sd card function

--- lm_car/src/main.cpp
+++ lm_car/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include "tiny_lcd.h"
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
-
+#include "radio.h"
+#include <RadioLib.h>
+#include "FS.h" 
+#include "SD.h"
+#include "SPI.h"
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 SFE_UBLOX_GNSS myGNSS;
+SX1276 radio = new Module(pin_cs, pin_dio0, pin_nrst, pin_dio1);
 
 void lilText(float data)
 {
@@ -54,6 +59,37 @@ float gpsStuff()
     Serial.println();
     return speed;
   }
+} // generates warning not retunringn anything in float return function
+
+// void transmitStuff(float speed)
+// {
+//   int radioStatus = -1;
+//   float speedtemp = gpsStuff() / 447.04; // convert  to mph
+//   lilText(speedtemp);
+
+//   radioStatus = radio.transmit("poop");
+//    if (radioStatus == RADIOLIB_ERR_NONE) {
+//     // the packet was successfully transmitted
+//     Serial.println(F(" success!"));
+
+//     // print measured data rate
+//     Serial.print(F("[SX1278] Datarate:\t"));
+//     Serial.print(radio.getDataRate());
+//     Serial.println(F(" bps"));
+
+//   }
+// }
+
+void logToSD(float speed,fs::FS &fs){
+  
+  File file = fs.open("/log.txt", FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    file.print(speed);file.print(",\n");
+    Serial.println("data sent to sd card");
+    file.close();
 }
 
 void setup()
@@ -66,20 +102,71 @@ void setup()
     Serial.println(F("SSD1306 allocation failed"));
   }
 
+   if(!SD.begin(16)){
+        Serial.println("Card Mount Failed");
+  }
+
+
   Serial.println("trying gps ");
   if (myGNSS.begin() == false) // Connect to the u-blox module using Wire port
   {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
   }
 
-  
-  
+  Serial.println("trying lora radio ");
+  // set up lora radio
+  //  carrier frequency:           915.0 MHz
+  //  bandwidth:                   500.0 kHz
+  //  spreading factor:            12 7
+  //  coding rate:                 5
+  //  sync word:                   0x14
+  //  output power:                30 dBm = 1 watt limit , 20dbm = .1 watt, sw limit is enabled need to disable
+  //  preamble length:             20 symbols
+  //  amplifier gain:              0 = auto gain, 1 (maximum gain)
+  int state = radio.begin(915.0, 500.0, 12, 5, 0x14, 20, 20, 0);
+  radio.setRfSwitchPins(pin_rx_enable, pin_tx_enable); //  15, 13
+  radio.setCurrentLimit(240);
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    Serial.println(F("success!"));
+  }
+  else
+  {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+
+ 
 }
+
 float speedtemp = -1;
+//what we give to radio to send out
+char output[256];
+//converting float to string to send as char array in output
+char speed[5]; // 150.5\0
+
 void loop()
 {
-  speedtemp = gpsStuff() / 447.04;
-  
+  int radioStatus = -1;
+  speedtemp = gpsStuff() / 447.04; // convert  to mph
   lilText(speedtemp);
+  dtostrf(speedtemp,-4,1,speed);
+  sprintf(output,"speed %s",speed); // a way to combine a bunch of data into a char array
+  Serial.print("size of payload ");
+  Serial.print(sizeof(output));
+  Serial.print("payload: ");
+  Serial.println(output);
+  logToSD(speedtemp,SD); //comenting out due to conflict with radio
+  radioStatus = radio.transmit(output);
+   if (radioStatus == RADIOLIB_ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F(" success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1278] Datarate:\t"));
+    Serial.print(radio.getDataRate());
+    Serial.println(F(" bps"));
+
+  }
   // put your main code here, to run repeatedly:
 }
